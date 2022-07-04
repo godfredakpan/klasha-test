1ba0c348d57b5816b25ea9071c9075d291c151da
Godfred Akpan <godfredakpan@gmail.com>
2022-07-04 14:55:17 +0100

    removed unwanted consoles

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React, { lazy } from 'react'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 const Layout = lazy(() => import('./layouts/Layout'))
 function App() {
-  console.log('App')
   return (
     <>
       <Router>
